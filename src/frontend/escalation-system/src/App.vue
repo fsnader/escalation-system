e7ca1ae21cdd05f6c82d926489501612dc101713
Rodolfo <rodolfo.leopoldino@leadr.com.br>
2021-01-31 20:04:05 -0300

    ajuste de logo, alteracao pra 30 segundos

--- src/frontend/escalation-system/src/App.vue
+++ src/frontend/escalation-system/src/App.vue
@@ -6,7 +6,7 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>XP Escalation System</v-toolbar-title>
+      <v-toolbar-title style=" font-family: Arial; font-size: 1.75rem"><span style="color: #ffd605">X</span>cale u<span style="color: #ffd605">P</span></v-toolbar-title>
     </v-app-bar>
 
     <v-main>
